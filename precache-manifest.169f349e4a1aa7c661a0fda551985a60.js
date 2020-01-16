77358b4b666cff99b2014d1ecf6f0d5da3d84612
Dorin-Ciora <52659995+Dorin-Ciora@users.noreply.github.com>
2020-01-16 07:08:51 +0200

    Updates

---
+++ precache-manifest.169f349e4a1aa7c661a0fda551985a60.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c43918c5281cd1284b9f1a19db884e80",
+    "url": "/TV-Lab/index.html"
+  },
+  {
+    "revision": "730fa1bef44f073f0e89",
+    "url": "/TV-Lab/static/css/main.608a89d3.chunk.css"
+  },
+  {
+    "revision": "113a075c7f90ffe274df",
+    "url": "/TV-Lab/static/js/2.6d323102.chunk.js"
+  },
+  {
+    "revision": "83b3d04082a8c50f224f3e213e6a5a45",
+    "url": "/TV-Lab/static/js/2.6d323102.chunk.js.LICENSE"
+  },
+  {
+    "revision": "730fa1bef44f073f0e89",
+    "url": "/TV-Lab/static/js/main.d34cf9b5.chunk.js"
+  },
+  {
+    "revision": "8f53b97aedde6c1e4217",
+    "url": "/TV-Lab/static/js/runtime-main.aff7231f.js"
+  },
+  {
+    "revision": "aaaab3fd2f948702b27c48224db54666",
+    "url": "/TV-Lab/static/media/avengers.aaaab3fd.jpg"
+  },
+  {
+    "revision": "8f35b89e9c9deadf26208449e92926eb",
+    "url": "/TV-Lab/static/media/interstelar.8f35b89e.jpg"
+  },
+  {
+    "revision": "462e11a90df1edb7fcb593ee9ff91e96",
+    "url": "/TV-Lab/static/media/movie4.462e11a9.jpg"
+  },
+  {
+    "revision": "1fe79cce955d95fc11d6b78c52291686",
+    "url": "/TV-Lab/static/media/movie6.1fe79cce.jpg"
+  },
+  {
+    "revision": "d88b383d200e3368ea1991b473b1ca0c",
+    "url": "/TV-Lab/static/media/rampage.d88b383d.jpg"
+  },
+  {
+    "revision": "fc4e6f11e40f85301f2db3241bc51c60",
+    "url": "/TV-Lab/static/media/thor.fc4e6f11.jpg"
+  }
+]);
